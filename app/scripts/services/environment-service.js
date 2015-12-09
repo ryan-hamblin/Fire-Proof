a59e4fccd28ed0f0b640114bbb47f28585922e64
Ryan Hamblin <ryanhca@gmail.com>
2015-12-09 07:57:28 -0600

    created env for firebase

---
+++ app/scripts/services/environment-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name rtfmApp.environmentService
+ * @description
+ * # environmentService
+ * Service in the rtfmApp.
+ */
+angular.module('rtfmApp')
+  .service('environmentService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ test/spec/services/environment-service.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: environmentService', function () {
+
+  // load the service's module
+  beforeEach(module('rtfmApp'));
+
+  // instantiate service
+  var environmentService;
+  beforeEach(inject(function (_environmentService_) {
+    environmentService = _environmentService_;
+  }));
+
+  it('should do something', function () {
+    expect(!!environmentService).toBe(true);
+  });
+
+});
